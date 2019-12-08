3e443d81ccac820f8064abaa1441aed4f461c61e
Rahul Chauhan <dellmyfriend@gmail.com>
2019-12-08 22:39:03 +0530

    added the environmental variables

--- main.py
+++ main.py
@@ -19,15 +19,15 @@ def task():
     realtime_data = ScrapComp().get_data()
     # print(realtime_data)
     # fb.delete('/mytestdata/','1')
-    res = fb.patch('/rr', realtime_data)
+    res = fb.patch('/rl', realtime_data)
     print("database updated", res)
 
 
 while True:
     print("started running .....")
     # schedule.run_pending()
-    # task()
-    print(DataBase_B, DataBase_R)
+    task()
+    # print(DataBase_B, DataBase_R)
     print("waiting for 60 second(s) .....")
     time.sleep(60)
     # task()
